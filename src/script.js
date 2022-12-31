43ef66543480ae0723cf78ad0374c225887e855b
kesviki <keseruviktoria@icloud.com>
2022-12-31 15:11:47 +0100

    Added current temperature, city. weather description, humidity, wind in JS

---
+++ src/script.js
@@ -0,0 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let currentTemperatureElement = document.querySelector("#currentTemperature");
+  currentTemperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "3980a7c8f2a782241a093131b099f993";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
