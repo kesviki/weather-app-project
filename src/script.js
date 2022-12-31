9f439673f509e3c73cdf9374e0bc30721ffbad7a
kesviki <keseruviktoria@icloud.com>
2022-12-31 15:40:34 +0100

    Added city

--- src/script.js
+++ src/script.js
@@ -39,6 +39,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "3980a7c8f2a782241a093131b099f993";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
